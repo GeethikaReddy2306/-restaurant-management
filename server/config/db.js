d30cbbb0685d909168bb1ca35dabf78563adbc9d
geethika <geethikareddy051@gmail.com>
2026-05-30 20:57:41 +0530

    deployement

--- server/config/db.js
+++ server/config/db.js
@@ -1,7 +1,4 @@
 const mongoose = require('mongoose');
-const dns=require('dns');
-require('dotenv').config();
-dns.setServers(['1.1.1.1','8.8.8.8']);
 const connectDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
